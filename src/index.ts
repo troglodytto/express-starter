02a0092d1f349734689a9df86cc8405e065d3a1d
Ephemeral Astronaut <troglodytto@gmail.com>
2022-03-31 18:28:20 +0530

    Add Server Startup Log

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import config from 'config';
 import app from './app';
 import connectDatabase from './setup';
 import socketServer from './socket';
+import logger from './utils/log';
 
 const PORT = config.get<number>('app.port') || 3000;
 
 async function main() {
   await connectDatabase(false);
-  const server = app.listen(PORT);
+  const server = app.listen(PORT, () => {
+    logger.info(`Server running on http://localhost:${PORT}`);
+  });
+
   socketServer.listen(server);
 }
 
